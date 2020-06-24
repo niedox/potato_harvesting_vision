84878051c5a40cd5ecab5eab8ceb40331d2e1fe5
Thibault Niederhauser <thibault.niederhauser@epfl.ch>
2020-06-24 12:10:41 +0200

    add code for taking pictures, better pose estim

--- SSD_on_RS.py
+++ SSD_on_RS.py
@@ -9,7 +9,9 @@ import RS_camera
 
 from utils import display_output
 from image_processing import ImageProcessing
-
+from compute_coor import get_dist
+from skimage.color import gray2rgb
+from imutils import grab_contours
 
 #CONSTANTS
 MODEL_NAME              = 'mobilenet_v1_2'
@@ -18,7 +20,7 @@ PATH_TO_LABEL_MAP       = 'label_map.pbtxt'
 NUM_CLASSES             = 1
 SEG_TYPE                = "edge" #can be "edge", "kmeans"
 THRESHOLD               = 0.5
-
+POSE_TYPE               = 0     #0 for PCA, 1 for ellipse fit
 
 """# Patch the location of gfile
 tf.gfile = tf.io.gfile
@@ -51,20 +53,58 @@ def process_vision():
 
                 boxes, classes, scores = ip.object_detection(detection_graph, sess, image_np)
                 boxes_s, classes_s, scores_s = ip.select_objects(boxes, classes, scores, THRESHOLD)
-                dist = RS_camera.get_dist(frames.get_depth_frame(), depth_scale, boxes_s)
+                dist = get_dist(frames.get_depth_frame(), depth_scale, boxes_s)
+                mask_im, box_h, box_w = ip.instance_seg(image_np, boxes_s)
 
-                mask_im, box_h, box_w = ip.instance_seg(colorized_depth, boxes_s)
-                display_output(image_np, boxes_s, classes_s, scores_s, category_index, dist, 'RGB')
-                display_output(colorized_depth, boxes_s, classes_s, scores_s, category_index, dist, 'Depth')
-                # display_output(mask_im, boxes_s, classes_s, scores_s, category_index, dist, 'Mask')
                 if mask_im != []:
                     buff = 0
                     for i in range(boxes_s.shape[0]):
-                        nb_pixels = box_h[i] * box_w[i]
+                        nb_pixels = int(box_h[i] * box_w[i])
+
                         mask_im_cur = mask_im[buff:(buff + nb_pixels)]
-                        mask_im_cur = np.reshape(mask_im_cur, (box_w[i], box_h[i]))
-                        cv2.imshow("mask " + str(i), mask_im_cur)
+                        mask_im_cur = np.reshape(mask_im_cur, (int(box_w[i]), int(box_h[i])))
+
+                        if POSE_TYPE == 0:
+                            v = ip.principal_axis(mask_im_cur)
+
+                            #mask_im_cur = gray2rgb(mask_im_cur)
+                            #yo = mask_im_cur.shape[0]
+                            #cv2.line(mask_im_cur, (0, yo), (int(100*v[0][1]), yo + int(100*v[0][0])),
+                            #        (0, 0, 255), thickness=2)
+                            xo = (boxes_s[i, 1] * image_np.shape[1]).astype(int)
+                            yo = (boxes_s[i, 0] * image_np.shape[0]).astype(int)
+                            print(v)
+                            cv2.line(image_np, (xo, yo), (xo + int(100*v[1]), yo + int(100 * v[0])),
+                                     (0, 0, 255), thickness=2)
+                            #cv2.imshow("mask " + str(i), mask_im_cur)
+
+                        elif POSE_TYPE == 1:
+                            _, binary_im = cv2.threshold(mask_im_cur,  0.5, 255, 0)
+                            #cnts = cv2.findContours(binary_im, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                            #cnts = grab_contours(cnts)
+                            #cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
+                            mask_im_cur = cv2.cvtColor(mask_im_cur, cv2.COLOR_GRAY2BGR)
+                            pt_x, pt_y, _ = np.where(mask_im_cur>200)
+                            pts = np.zeros((len(pt_x), 2))
+                            pts[:,0] = pt_x
+                            pts[:,1] = pt_y
+                            pts = pts.astype(int)
+                            if pts.shape[0]>5:
+                                el = cv2.fitEllipse(pts)
+                                cv2.ellipse(mask_im_cur, el, (255, 0, 255), 2)
+                                x0 = (boxes_s[i, 1] * image_np.shape[1]).astype(int)
+                                y0 = (boxes_s[i, 0] * image_np.shape[0]).astype(int)
+
+
+                                el = ((el[0][0] + x0, el[0][1] + y0), el[1], el[2])
+                                cv2.ellipse(image_np, el,
+                                         (255, 0, 255), thickness=2)
+                            #cv2.drawContours(mask_im_cur, cnts, -1, (0, 255, 0), 3)
+                            #cv2.imshow("mask " + str(i), mask_im_cur)
                         buff = buff + nb_pixels
+
+                display_output(image_np, boxes_s, classes_s, scores_s, category_index, dist, 'RGB')
+                display_output(colorized_depth, boxes_s, classes_s, scores_s, category_index, dist, 'Depth')
 
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     cv2.destroyAllWindows()
--- image_processing.py
+++ image_processing.py
@@ -4,8 +4,9 @@ import tensorflow as tf
 
 from object_detection.utils import label_map_util
 from scipy import ndimage
-from skimage.color import rgb2gray
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
 
 # Patch the location of gfile
 tf.gfile = tf.io.gfile
@@ -90,33 +91,45 @@ class ImageProcessing():
         (h, w) = image.shape[:2]
         out_l = []
         box_h, box_w = [], []
+        margin = 0
         # crop image
         for i in range(boxes_s.shape[0]):
             xmin = (boxes_s[i, 0] * h).astype(int)
-            ymin = (boxes_s[i, 1] * w).astype(int)
-            xmax = (boxes_s[i, 2] * h).astype(int)
-            ymax = (boxes_s[i, 3] * w).astype(int)
+            if xmin > margin:
+                xmin = xmin - margin
+            ymin = (boxes_s[i, 1] * w - margin).astype(int)
+            if ymin > margin:
+                ymin = ymin - margin
+            xmax = (boxes_s[i, 2] * h + margin).astype(int)
+            if xmax < w-margin:
+                xmax = xmax + margin
+            ymax = (boxes_s[i, 3] * w + margin).astype(int)
+            if ymax < h-margin:
+                ymax = ymax + margin
             crop = image[xmin:xmax, ymin:ymax]
 
             # edge detection
             if self.seg_type == "edge":
                 print(self.kernel, 'is a laplacian kernel')
                 # converting to grayscale
-                gray = rgb2gray(crop)
+                #gray = rgb2gray(crop)
+                gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+                gray = cv2.bilateralFilter(gray, 11, 17, 17)
+                #cv2.imshow("GRAY", gray)
+
                 if out_l == []:
-                    out_l = ndimage.convolve(gray, self.kernel, mode='reflect')
+                    #out_l = ndimage.convolve(gray, self.kernel, mode='reflect')
+                    out_l = cv2.Canny(gray, 30, 200)
                     out_l = out_l.reshape(out_l.shape[0] * out_l.shape[1])
                     box_h = [(ymax - ymin)]
                     box_w = [(xmax - xmin)]
                 else:
-                    edge_im = ndimage.convolve(gray, self.kernel, mode='reflect')
+                    #edge_im = ndimage.convolve(gray, self.kernel, mode='reflect')
+                    edge_im = cv2.Canny(gray, 30, 200)
                     linear_edge_im = edge_im.reshape(edge_im.shape[0] * edge_im.shape[1])
                     box_h = np.append(box_h, (ymax - ymin))
                     box_w = np.append(box_w, (xmax - xmin))
-                    print("shape pre", np.shape(out_l))
-                    print("shape lin", np.shape(edge_im))
                     out_l = np.append(out_l, linear_edge_im)
-                    print("shape", np.shape(out_l))
 
             elif self.seg_type == "kmeans":
                 # clustering
@@ -124,7 +137,13 @@ class ImageProcessing():
                 pic_n = image.reshape(image.shape[0] * image.shape[1], image.shape[2])
                 kmeans = KMeans(n_clusters=5, random_state=0).fit(pic_n)
                 pic2show = kmeans.cluster_centers_[kmeans.labels_]
-                out_l = pic2show.reshape(image.shape[0], image.shape[1], image.shape[2])
+                out_cur = pic2show.reshape(image.shape[0], image.shape[1], image.shape[2])
+
+                linear_out_cur = out_cur.reshape(out_cur.shape[0] * out_cur.shape[1] * out_cur.shape[2])
+                box_h = np.append(box_h, (ymax - ymin))
+                box_w = np.append(box_w, (xmax - xmin))
+                out_l = np.append(out_l, linear_out_cur)
+
 
         # region based
         """gray_r = gray.reshape(gray.shape[0] * gray.shape[1])
@@ -139,4 +158,63 @@ class ImageProcessing():
 
         return out_l, box_h, box_w
         # plt.imshow(out_l, cmap='gray')
+
+    def principal_axis(self, crop):
+        X_row, X_col = np.where(crop > 200)
+
+        X = np.zeros((len(X_row), 2))
+        X[:, 0] = X_row
+        X[:, 1] = X_col
+
+        mean = np.empty((0))
+
+        if len(X) > 0:
+            _, eigenvectors, _ = cv2.PCACompute2(X, mean)
+            principal_axis = eigenvectors[0, :]
+        else:
+            principal_axis = [0,0]
+
+        #PCA with sklearn
+        """pca = PCA(n_components=1)
+        if X.shape[0] > 0:
+            pca.fit(X)
+            principal_axis = pca.components_
+        else:
+            principal_axis = [[0,0]]"""
+
+        return principal_axis
+
+    def get_binary_crops(self, image, boxes_s):
+        (h, w) = image.shape[:2]
+        out_l = []
+        box_h, box_w = [], []
+        # crop image
+        for i in range(boxes_s.shape[0]):
+            xmin = (boxes_s[i, 0] * h).astype(int)
+            ymin = (boxes_s[i, 1] * w).astype(int)
+            xmax = (boxes_s[i, 2] * h).astype(int)
+            ymax = (boxes_s[i, 3] * w).astype(int)
+            crop = image[xmin:xmax, ymin:ymax]
+
+            # Convert image to grayscale
+            gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+            #cv2.imshow("GRAY", gray)
+            # Convert image to binary
+
+
+            if out_l == []:
+                # out_l = ndimage.convolve(gray, self.kernel, mode='reflect')
+                _ , out_l = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                out_l = out_l.reshape(out_l.shape[0] * out_l.shape[1])
+                box_h = [(ymax - ymin)]
+                box_w = [(xmax - xmin)]
+            else:
+                # edge_im = ndimage.convolve(gray, self.kernel, mode='reflect')
+                _ , bin_im = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                linear_edge_im = bin_im.reshape(bin_im.shape[0] * bin_im.shape[1])
+                box_h = np.append(box_h, (ymax - ymin))
+                box_w = np.append(box_w, (xmax - xmin))
+                out_l = np.append(out_l, linear_edge_im)
+
+        return out_l, box_h, box_w
 
